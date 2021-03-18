f88b7771f5ec306db041488f1c532918c6a4a99f
maxassis <max.assis@ymail.com>
2021-03-18 14:54:19 -0300

    iniciando pagina de busca

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,93 +8,12 @@ import Conditions from "../../components/Conditions";
 import Forecast from "../../components/Forecast/index";
 import api, { key } from "../../services/api";
 
-const mylist = [
-  {
-    date: "17/03",
-    weekday: "Qua",
-    max: 29,
-    min: 18,
-    description: "Tempestades",
-    condition: "storm",
-  },
-  {
-    date: "18/03",
-    weekday: "Qui",
-    max: 27,
-    min: 19,
-    description: "Tempestades",
-    condition: "storm",
-  },
-  {
-    date: "19/03",
-    weekday: "Sex",
-    max: 26,
-    min: 19,
-    description: "Tempestades",
-    condition: "storm",
-  },
-  {
-    date: "20/03",
-    weekday: "Sáb",
-    max: 27,
-    min: 17,
-    description: "Tempestades isoladas",
-    condition: "storm",
-  },
-  {
-    date: "21/03",
-    weekday: "Dom",
-    max: 28,
-    min: 18,
-    description: "Ensolarado",
-    condition: "clear_day",
-  },
-  {
-    date: "22/03",
-    weekday: "Seg",
-    max: 29,
-    min: 19,
-    description: "Parcialmente nublado",
-    condition: "cloudly_day",
-  },
-  {
-    date: "23/03",
-    weekday: "Ter",
-    max: 29,
-    min: 18,
-    description: "Parcialmente nublado",
-    condition: "cloudly_day",
-  },
-  {
-    date: "24/03",
-    weekday: "Qua",
-    max: 30,
-    min: 18,
-    description: "Ensolarado com muitas nuvens",
-    condition: "cloudly_day",
-  },
-  {
-    date: "25/03",
-    weekday: "Qui",
-    max: 25,
-    min: 19,
-    description: "Tempestades",
-    condition: "storm",
-  },
-  {
-    date: "26/03",
-    weekday: "Sex",
-    max: 27,
-    min: 17,
-    description: "Parcialmente nublado",
-    condition: "cloudly_day",
-  },
-];
-
 export default function Home() {
   const [errorMsg, setErrorMsg] = useState(null);
   const [loading, setLoading] = useState(true);
   const [weather, setWeather] = useState([]);
+  const [icon, setIcon] = useState({ name: "cloud", color: "#FFF" });
+  const [background, setBackground] = useState(["#1ed6ff", "#97c1ff"]);
 
   useEffect(() => {
     (async () => {
@@ -111,20 +30,52 @@ export default function Home() {
         `/weather?key=${key}&lat=${location.coords.latitude}&lon=${location.coords.longitude}`
       );
       setWeather(response.data);
-      console.log(response.data);
+
+      if (response.data.results.currently === "noite") {
+        setBackground(["#0c3741", "#0f2f61"]);
+      }
+
+      switch (response.data.results.condition_slug) {
+        case "clear_day":
+          setIcon({ name: "partly-sunny", color: "##FFb300" });
+          break;
+        case "rain":
+          setIcon({ name: "rainy", color: "#FFF" });
+          break;
+        case "storm":
+          setIcon({ name: "rainy", color: "#FFF" });
+          break;
+      }
+
+      setLoading(false);
     })();
   }, []);
+
+  if (loading) {
+    return (
+      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <Text
+          style={{
+            fontSize: 17,
+            fontStyle: "italic",
+          }}
+        >
+          Carregando dados...
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <Menu />
-      <Header />
-      <Conditions />
+      <Header background={background} weather={weather} icon={icon} />
+      <Conditions weather={weather} />
 
       <FlatList
         horizontal={true}
         style={styles.list}
-        data={mylist}
+        data={weather.results.forecast}
         contentContainerStyle={{ paddingBottom: "5%" }}
         keyExtractor={(item) => item.date}
         renderItem={({ item }) => <Forecast data={item} />}
